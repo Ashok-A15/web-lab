1f595d63857049397b713873c930cf4a69e249f8
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2025-09-03 15:23:12 +0530

    Create 10a.php

---
+++ 10a.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Today's Date</title>
+</head>
+<body>
+
+    <h1>Welcome!</h1>
+    <p>
+        <?php
+            // Display today's date in dd-mm-yyyy format
+            echo "Today's date is: " . date("d-m-Y");
+        ?>
+    </p>
+
+</body>
+</html>
